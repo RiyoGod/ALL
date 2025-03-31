d84044b042ceed663c0400126cdd289f1ea8c3b9
RiyoGod <godsxrio@gmail.com>
2025-03-31 18:31:49 +0530

    Update sticker.py

--- AnonXMusic/plugins/tools/sticker.py
+++ AnonXMusic/plugins/tools/sticker.py
@@ -84,17 +84,15 @@ async def _packkang(app :app,message):
 @app.on_message(filters.command(["stickerid","stid"]))
 async def sticker_id(app: app, msg):
     if not msg.reply_to_message:
-        await msg.reply_text("<b>Reply to a sticker</b>", parse_mode="html")        
+        await msg.reply_text("Reply to a sticker")        
     elif not msg.reply_to_message.sticker:
-        await msg.reply_text("<b>Reply to a sticker</b>", parse_mode="html")        
+        await msg.reply_text("Reply to a sticker")        
     st_in = msg.reply_to_message.sticker
     await msg.reply_text(f"""
-<b>⊹ sᴛɪᴄᴋᴇʀ ɪɴғᴏ ⊹</b>
-
-<b>⊚ sᴛɪᴄᴋᴇʀ ɪᴅ:</b> <code>{st_in.file_id}</code>
-
-<b>⊚ sᴛɪᴄᴋᴇʀ ᴜɴɪǫᴜᴇ ɪᴅ:</b> <code>{st_in.file_unique_id}</code>
-""", parse_mode="html")
+⊹ <u>**sᴛɪᴄᴋᴇʀ ɪɴғᴏ**</u> ⊹
+**⊚ sᴛɪᴄᴋᴇʀ ɪᴅ **: `{st_in.file_id}`\n
+**⊚ sᴛɪᴄᴋᴇʀ ᴜɴɪǫᴜᴇ ɪᴅ **: `{st_in.file_unique_id}`
+""")
 
 
 #####
